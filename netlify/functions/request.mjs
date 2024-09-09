f20bffc2dbbb77ee668bf398743686b8e67508ee
Yoan CHALENCON <81905759+CHALENCON-Yoan@users.noreply.github.com>
2024-09-09 15:54:21 +0000

    Use default path

--- netlify/functions/request.mjs
+++ netlify/functions/request.mjs
@@ -1,27 +1,23 @@
 export default async (req, context) => {
-  return new Response("Hello, world!");
+  try {
+    console.log("test");
+    console.log(req);
+    
+    const url = new URL(req.url);
+    url.host = "ade-uga-ro-vs.grenet.fr";
+    url.pathname = "/jsp/webapi";
+    
+    const res = await fetch(url);
+    res.headers.set("Access-Control-Allow-Origin", "*");
+
+    return new Response(res.body, {
+        headers: res.headers,
+    });
+  } catch (e) {
+    console.error(e);
+    return new Response("Une erreur est survenue", { status: 400 });
+  }
 };
-
-// export default async (req, context) => {
-//   try {
-//     console.log("test");
-//     console.log(req);
-    
-//     const url = new URL(req.url);
-//     url.host = "ade-uga-ro-vs.grenet.fr";
-//     url.pathname = "/jsp/webapi";
-    
-//     const res = await fetch(url);
-//     res.headers.set("Access-Control-Allow-Origin", "*");
-
-//     return new Response(res.body, {
-//         headers: res.headers,
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     return new Response("Une erreur est survenue", { status: 400 });
-//   }
-// };
 
 // export const config = {
 //   path: "/request",
