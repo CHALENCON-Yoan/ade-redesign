514e358b6e03b9b0d5665c949d873cb70ea6e0bc
Yoan CHALENCON <chalenconyoanpro@gmail.com>
2024-10-30 11:21:42 +0100

    ✏️ Update error mail body

--- pop-ups.js
+++ pop-ups.js
@@ -2,7 +2,7 @@ function displayError(code, error) {
   const emailLink = document.querySelector("#linkForErrorMail");
   emailLink.setAttribute(
     "href",
-    `mailto:${DEV_MAIL}?subject=[${code}-${error.status}]%20J%27ai%20rencontré%20une%20erreur%20sur%20${document.location.host}&body=J%27ai%20rencontré%20une%20erreur%20lors%20de%20ma%20navigation%20sur%20le%20site%20${document.location.origin}.%20J%27étais%20sur%20la%20page%20${document.location.pathname}%20et%20j%27ai%20obtenu%20une%20erreur%20${error.status}%20avec%20le%20code%20${code}.`
+    `mailto:${DEV_MAIL}?subject=%5B${code}-${error.status}%5D%20J%27ai%20rencontré%20une%20erreur%20sur%20${document.location.host}&body=Bonjour%2C%0AJ%27ai%20rencontré%20une%20erreur%20lors%20de%20ma%20navigation%20sur%20le%20site%20${document.location.origin}.%0AJ%27étais%20sur%20la%20page%20${document.location.pathname}%20et%20j%27ai%20obtenu%20une%20erreur%20${error.status}%20avec%20le%20code%20${code}.`
   );
 
   const errorCodeDisplay = document.querySelector("#errorCode");
