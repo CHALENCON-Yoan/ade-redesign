e55f56193233f2f7d281868dc87e1ce8fc2b3ea7
Yoan CHALENCON <81905759+CHALENCON-Yoan@users.noreply.github.com>
2025-06-26 10:06:11 +0000

    Passage au nouveau projet (2025-2026 en cours de préparation)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const DEV_MAIL = "yoan@ade-edt.fr";
 const LOCALHOST = false;
 const BASE_URL = "/request";
-const PORJECT_ID = "3";
+const PORJECT_ID = "1";
 const PROJECT_DATA =
   "08281677eda1e7f64e2d372185934117103d4fbbeeae8daf5feb097f6d188a31fa91aec4c63f8a2413abf3eb0200af77893b24c3f0ea376490a4df59bbd7d7dac1ce1c636bfed8c261cddb98478bb779bd1ac4646cfd5693729bf4b8e78c59567ae83326afa226fea9096e273cc145b91561414a3d67e17424890a31962d0e88c0e824b72ad3aea61e43ef842c9e91c3,1";
-const MAX_DATE = new Date(1753912800000);
+const MAX_DATE = new Date(1785448800000);
 
 let resourcesMap = {};
 let weekMap = {};
