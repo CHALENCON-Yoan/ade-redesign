d2c848f92d544a2e91e1aaddd094a4d03b34f6d3
Yoan CHALENCON <chalenconyoanpro@gmail.com>
2025-08-21 11:23:44 +0200

    :pencil2: Edit PROJECT_ID name (PORJECT_ID)

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ let LOCALHOST =
 const BASE_URL = LOCALHOST ? "https://dev.ade-edt.fr/localRequest" : "/request";
 LOCALHOST = false;
 // "https://cors-anywhere.herokuapp.com/https://ade-uga-ro-vs.grenet.fr/jsp/webapi";
-const PORJECT_ID = "1";
+const PROJECT_ID = "1";
 const PROJECT_DATA =
   "51278da58f6a6c7ad5fcd549aa2f4a9cd0fdf1bdade3448b5e9400c4e4311a3aaf5892c4f26a6a95b2be9815c8d857a2533823d5748e25e65116564f1a3e9953bfee61b8973d94f85031bb73a57c6000da7dc82411a94d7de68c82ab46db4fa8,1";
 const MAX_DATE = new Date('2026-07-31');
