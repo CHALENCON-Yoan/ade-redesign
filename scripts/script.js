2169f6149b486909cd530093cf721583011cd63a
Yoan CHALENCON <chalenconyoanpro@gmail.com>
2025-08-22 11:45:14 +0200

    ✨ Add announce/information message when onload

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,55 @@
 const DEV_MAIL = "yoan@ade-edt.fr";
 let LOCALHOST =
-    location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "192.168.1.10";
+    location.hostname === "localhost" ||
+    location.hostname === "127.0.0.1" ||
+    location.hostname === "192.168.1.10";
 const BASE_URL = LOCALHOST ? "https://dev.ade-edt.fr/localRequest" : "/request";
+const API_URL = "https://api.ade-edt.fr";
 LOCALHOST = false;
 // "https://cors-anywhere.herokuapp.com/https://ade-uga-ro-vs.grenet.fr/jsp/webapi";
 const PROJECT_ID = "1";
 const PROJECT_DATA =
-  "51278da58f6a6c7ad5fcd549aa2f4a9cd0fdf1bdade3448b5e9400c4e4311a3aaf5892c4f26a6a95b2be9815c8d857a2533823d5748e25e65116564f1a3e9953bfee61b8973d94f85031bb73a57c6000da7dc82411a94d7de68c82ab46db4fa8,1";
-const MAX_DATE = new Date('2026-07-31');
+    "51278da58f6a6c7ad5fcd549aa2f4a9cd0fdf1bdade3448b5e9400c4e4311a3aaf5892c4f26a6a95b2be9815c8d857a2533823d5748e25e65116564f1a3e9953bfee61b8973d94f85031bb73a57c6000da7dc82411a94d7de68c82ab46db4fa8,1";
+const MAX_DATE = new Date("2026-07-31");
 
 let resourcesMap = {};
 let weekMap = {};
 
 window.addEventListener("load", async () => {
-  // Load Lottie file
-  loadLottie();
+    // Load Lottie file
+    loadLottie();
 
-  // Update Copyright Date
-  copyrightDate();
+    // Update Copyright Date
+    copyrightDate();
 
-  // Set the mode
-  setMode(localStorage.getItem("mode"));
+    // Set the mode
+    setMode(localStorage.getItem("mode"));
 
-  // Allow to switch the mode
-  switchMode();
+    // Allow to switch the mode
+    switchMode();
 
-  // Allow to open and close the sidebar
-  sidebar();
+    // Allow to open and close the sidebar
+    sidebar();
 
-  // Set the correct display (week/day)
-  setWeekDayDisplay();
+    // Set the correct display (week/day)
+    setWeekDayDisplay();
 
-  // Display the schedule grid
-  displayScheduleGrid();
+    // Display the schedule grid
+    displayScheduleGrid();
 
-  // Make the datalist for resources
-  loader(true);
-  await makeDatalist();
+    // Show announce
+    displayAnnounce(true);
 
-  // Get week map
-  await getCalendar();
+    // Make the datalist for resources
+    loader(true);
+    await makeDatalist();
 
-  // Restore resource value, set the next lesson date and display next lessons
-  await restoreResourceValue();
-  loader(false);
+    // Get week map
+    await getCalendar();
 
-  loadEventListeners();
+    // Restore resource value, set the next lesson date and display next lessons
+    await restoreResourceValue();
+    loader(false);
+
+    loadEventListeners();
 });
