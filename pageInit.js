62abf6a86710902b2d26d67e4024bea9121d9b71
Yoan CHALENCON <contact@yoan-chalencon.fr>
2025-09-16 22:06:05 +0200

    🔥 End of service

--- pageInit.js
+++
@@ -1,8 +0,0 @@
-function copyrightDate() {
-  const copyrightDateSpan = document.querySelector("#copyrightDate");
-  const date = new Date();
-  if (date.getFullYear() != copyrightDateSpan.textContent) {
-    copyrightDateSpan.textContent =
-      copyrightDateSpan.textContent + " - " + date.getFullYear();
-  }
-}
--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 window.addEventListener("load", () => {
-  // Update Copyright Date
-  copyrightDate();
+    // Set the mode
+    setMode(localStorage.getItem("mode"));
 
-  // Set the mode
-  setMode(localStorage.getItem("mode"));
-
-  // Allow to switch the mode
-  switchMode();
-
-  if (document.title.startsWith("Accueil")) {
-    // Set the hover effect on plus
-    plusHover();
-  }
+    // Allow to switch the mode
+    switchMode();
 });
