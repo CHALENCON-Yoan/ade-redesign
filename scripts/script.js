0cd8994869b11493dcec2b27aa9d8216cf1e1f32
Yoan CHALENCON <contact@yoan-chalencon.fr>
2026-03-24 13:45:35 +0100

    🔐 Update token for data project

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ LOCALHOST = false;
 // "https://cors-anywhere.herokuapp.com/https://ade-uga-ro-vs.grenet.fr/jsp/webapi";
 const PROJECT_ID = "1";
 const PROJECT_DATA =
-    "51278da58f6a6c7ad5fcd549aa2f4a9cd0fdf1bdade3448b5e9400c4e4311a3aaf5892c4f26a6a95b2be9815c8d857a2533823d5748e25e65116564f1a3e9953bfee61b8973d94f85031bb73a57c6000da7dc82411a94d7de68c82ab46db4fa8,1";
+    "08281677eda1e7f64e2d372185934117103d4fbbeeae8daf5feb097f6d188a31fa91aec4c63f8a2413abf3eb0200af7710df8dec5d859664f2e9534343758872ea63fdddf2e0bb3bc6a427c07829845b2adf41a8f1c053f79442ee207709b605e9b4141706c3679ed859fefe34f0d7ea,1";
 const MAX_DATE = new Date("2026-07-31");
 
 let resourcesMap = {};
